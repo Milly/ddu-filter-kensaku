3c5eca9f707a5f3e1b395103d321c09885cb1748
Milly <milly.ca@gmail.com>
2023-01-17 10:04:40 +0900

    feat: Initial import

---
+++ README.md
@@ -0,0 +1,51 @@
+# ddu-filter-kensaku
+
+Migemo matcher for ddu.vim
+
+[Migemo][] を利用してローマ字入力により [ddu.vim][] のアイテムから日本語文字列をマッチングします。
+
+## Required
+
+以下のプラグインに依存します。
+
+- [denops.vim][]
+- [ddu.vim][]
+- [kensaku.vim][]
+
+## Installation
+
+1. [Deno][] をインストールします。
+2. [vim-plug][] などを利用してプラグインをインストールします。
+
+```
+Plug 'vim-denops/denops.vim'
+Plug 'Shougo/ddu.vim'
+Plug 'lambdalisue/kensaku.vim'
+Plug 'Milly/ddu-filter-kensaku'
+```
+
+## Configuration
+
+ddu.vim の設定を行います。
+
+```vim
+call ddu#custom#patch_global('sourceOptions', #{
+      \  _: #{
+      \    matchers: ['matcher_kensaku'],
+      \  },
+      \})
+
+" Option: Enable highlight matched text
+call ddu#custom#patch_global('filterParams', #{
+    \  matcher_kensaku: #{
+    \    highlightMatched: 'Search',
+    \  },
+    \})
+```
+
+[Deno]: https://deno.land/
+[Migemo]: http://0xcc.net/migemo/
+[ddu.vim]: https://github.com/Shougo/ddu.vim
+[denops.vim]: https://github.com/vim-denops/denops.vim
+[kensaku.vim]: https://github.com/lambdalisue/kensaku.vim
+[vim-plug]: https://github.com/junegunn/vim-plug
---
+++ denops/@ddu-filters/matcher_kensaku.ts
@@ -0,0 +1,79 @@
+import {
+  BaseFilter,
+  type DduItem,
+} from "https://deno.land/x/ddu_vim@v2.2.0/types.ts";
+import type { FilterArguments } from "https://deno.land/x/ddu_vim@v2.2.0/base/filter.ts";
+
+type Params = {
+  highlightMatched: string;
+};
+
+export class Filter extends BaseFilter<Params> {
+  override params(): Params {
+    return {
+      highlightMatched: "",
+    };
+  }
+
+  override async filter(
+    { denops, input, items, sourceOptions, filterParams }: FilterArguments<
+      Params
+    >,
+  ): Promise<DduItem[]> {
+    input = input.trim();
+    if (input === "") return items;
+
+    if (sourceOptions.ignoreCase) {
+      input = input.toLowerCase();
+    }
+
+    const patterns = await Promise.all(
+      input.split(/\s+/).map((text) =>
+        denops.dispatch("kensaku", "query", text) as Promise<string>
+      ),
+    );
+    const patternFlags = [
+      sourceOptions.ignoreCase ? "i" : "",
+      "g",
+    ].join("");
+    const matchers = patterns.map((pattern) =>
+      new RegExp(pattern, patternFlags)
+    );
+
+    items = matchers.reduce(
+      (items, matcher) =>
+        items.filter(({ matcherKey }) => new RegExp(matcher).test(matcherKey)),
+      items,
+    );
+
+    if (filterParams.highlightMatched !== "") {
+      // Highlight matched text
+      items = items.map(
+        (item) => {
+          const display = item.display ?? item.word;
+          const highlights = item.highlights?.filter(({ name }) =>
+            name != "matched"
+          ) ?? [];
+          const matches = matchers.flatMap((matcher) =>
+            Array.from(display.matchAll(new RegExp(matcher)))
+          );
+          highlights.push(
+            ...matches.map((m) => ({
+              name: "matched",
+              "hl_group": filterParams.highlightMatched,
+              col: getByteLength(display.slice(0, m.index ?? 0)) + 1,
+              width: getByteLength(m[0]),
+            })),
+          );
+          return { ...item, highlights };
+        },
+      );
+    }
+
+    return items;
+  }
+}
+
+function getByteLength(s: string): number {
+  return (new TextEncoder()).encode(s).length;
+}
